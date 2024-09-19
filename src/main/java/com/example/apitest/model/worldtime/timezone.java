c3b7533574554c6351a23c7127d1eb12adacdb8f
ytallo <ytallo4gamer@gmail.com>
2024-09-19 17:12:50 -0300

    Model timezone criado

---
+++ src/main/java/com/example/apitest/model/worldtime/timezone.java
@@ -0,0 +1,30 @@
+package com.example.apitest.model.worldtime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.OffsetDateTime;
+
+
+@Entity
+@Data
+public class timezone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String utc_offset;
+    @Column(nullable = false)
+    private String timezone;
+    @Column(nullable = false)
+    private int day_of_week;
+    @Column(nullable = false)
+    private int day_of_year;
+    @Column(nullable = false)
+    private OffsetDateTime datetime;
+    @Column(nullable = false)
+    private OffsetDateTime utc_datetime;
+    @Column(nullable = false)
+    private long unixtime;
+    @Column(nullable = false)
+    private int week_number;
+}
