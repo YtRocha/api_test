037095fa58ebe7850fb670f53b73b5a593d408f5
ytallo <ytallo4gamer@gmail.com>
2024-09-19 18:03:55 -0300

    Correções na service de timezone

--- src/main/java/com/example/apitest/service/worldtime/TimezoneService.java
+++ src/main/java/com/example/apitest/service/worldtime/TimezoneService.java
@@ -1,23 +1,44 @@
 package com.example.apitest.service.worldtime;
 
 import com.example.apitest.model.worldtime.Timezone;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
+/**
+ * A service para Timezone
+ */
+@Service
 public class TimezoneService {
+
+    /**
+     * Instancia de webclient para realizar as chamadas de api externa para a api de timezone
+     */
     private final WebClient webClient;
 
+    /**
+     * O Construtor
+     *
+     * @param webClient
+     */
     public TimezoneService(WebClient webClient) {
         this.webClient = webClient;
     }
 
-    public Mono<Timezone> getTimezoneDisponiveis() {
-
-        String apiUrl = "http://worldtimeapi.org/api/timezone";
-
-        return webClient.get()
-                .uri(apiUrl)
+    /**
+     * função que solicita a api externa as timezones disponiveis
+     *
+     * @return Lista de strings de timezones disponiveis
+     */
+    public List<String> getTimezones() {
+        Mono<List<String>> timezonesMono = webClient.get()
+                .uri("https://worldtimeapi.org/api/timezone")
                 .retrieve()
-                .bodyToMono(Timezone.class);
+                .bodyToMono(new ParameterizedTypeReference<List<String>>() {
+                });
+        return timezonesMono.block();
     }
 }
