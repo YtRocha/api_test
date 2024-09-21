36c990b259b0d40ecf55f5ba71c19dac722e2962
ytallo <ytallo4gamer@gmail.com>
2024-09-21 16:03:34 -0300

    Criação do timezoneDto

---
+++ src/main/java/com/example/apitest/dto/worldtime/TimezoneDto.java
@@ -0,0 +1,19 @@
+package com.example.apitest.dto.worldtime;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class TimezoneDto {
+
+    private String utc_offset;
+    private String timezone;
+    private int day_of_week;
+    private int day_of_year;
+    private OffsetDateTime datetime;
+    private OffsetDateTime utc_datetime;
+    private long unixtime;
+    private int week_number;
+}
