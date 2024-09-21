6dae159b763cd60bf26782d857a16383254830be
ytallo <ytallo4gamer@gmail.com>
2024-09-21 16:04:33 -0300

    criação das rotas de consumo da worldtime

--- src/main/java/com/example/apitest/controller/worldtime/TimezoneController.java
+++ src/main/java/com/example/apitest/controller/worldtime/TimezoneController.java
@@ -1,12 +1,12 @@
 package com.example.apitest.controller.worldtime;
 
+import com.example.apitest.dto.worldtime.TimezoneDto;
 import com.example.apitest.service.worldtime.TimezoneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -38,6 +38,38 @@ public class TimezoneController {
     @GetMapping
     public List<String> getTimezones() {
         return timezoneService.getTimezones();
+    }
+
+    /**
+     * Rota Get que retorna a timezone escolhida e salva no banco de dados
+     *
+     * @param zone
+     * @param zone2
+     * @param zone3
+     * @return timezone selecionada
+     */
+    @RequestMapping(value = {
+            "/{zone}",
+            "/{zone}/{zone_2}",
+            "/{zone}/{zone_2}/{zone_3}"
+    }, method = RequestMethod.GET)
+    public ResponseEntity<TimezoneDto> getTimezone(
+            @PathVariable("zone") String zone,
+            @PathVariable(value = "zone_2", required = false) String zone2,
+            @PathVariable(value = "zone_3", required = false) String zone3) {
+
+
+        String fullZone = zone;
+        if (zone2 != null) {
+            fullZone += "/" + zone2;
+        }
+        if (zone3 != null) {
+            fullZone += "/" + zone3;
+        }
+
+        // Chama o serviço com a zona completa
+        TimezoneDto timezoneDto = timezoneService.getTimezone(fullZone);
+        return new ResponseEntity<>(timezoneDto, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/apitest/service/worldtime/TimezoneService.java
+++ src/main/java/com/example/apitest/service/worldtime/TimezoneService.java
@@ -1,11 +1,16 @@
 package com.example.apitest.service.worldtime;
 
+import com.example.apitest.dto.worldtime.TimezoneDto;
 import com.example.apitest.model.worldtime.Timezone;
+import com.example.apitest.repository.worldtime.TimezoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriUtils;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -18,6 +23,9 @@ public class TimezoneService {
      * Instancia de webclient para realizar as chamadas de api externa para a api de timezone
      */
     private final WebClient webClient;
+
+    @Autowired
+    private TimezoneRepository timezoneRepository;
 
     /**
      * O Construtor
@@ -40,5 +48,29 @@ public class TimezoneService {
                 .bodyToMono(new ParameterizedTypeReference<List<String>>() {
                 });
         return timezonesMono.block();
+    }
+
+    public TimezoneDto getTimezone(String zone) {
+
+
+        Mono<TimezoneDto> timezoneMono = webClient.get()
+                .uri("https://worldtimeapi.org/api/timezone/" + zone)
+                .retrieve()
+                .bodyToMono(TimezoneDto.class);
+
+        TimezoneDto timezoneDto = timezoneMono.block();
+        Timezone timezone = new Timezone();
+
+        timezone.setTimezone(timezoneDto.getTimezone());
+        timezone.setDatetime(timezoneDto.getDatetime());
+        timezone.setUnixtime(timezoneDto.getUnixtime());
+        timezone.setUtc_datetime(timezoneDto.getUtc_datetime());
+        timezone.setUtc_offset(timezoneDto.getUtc_offset());
+        timezone.setDay_of_week(timezoneDto.getDay_of_week());
+        timezone.setDay_of_year(timezoneDto.getDay_of_year());
+        timezone.setWeek_number(timezoneDto.getWeek_number());
+
+        timezoneRepository.save(timezone);
+        return timezoneDto;
     }
 }
