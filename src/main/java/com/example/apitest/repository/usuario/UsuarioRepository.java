ffe9dbce2dc98eef81198d170c41e7e7a06a53d8
ytallo <ytallo4gamer@gmail.com>
2024-09-26 22:46:04 -0300

    Adição de um findbylogin no repositorio de usuario

--- src/main/java/com/example/apitest/repository/usuario/UsuarioRepository.java
+++ src/main/java/com/example/apitest/repository/usuario/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.example.apitest.model.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByLogin(String login);
 }
